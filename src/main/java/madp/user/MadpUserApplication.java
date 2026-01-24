fbe8035945965b2b1a15dacdf673770e70161d11
Lian08 <lian080408@gmail.com>
2026-01-24 19:09:07 +0900

    Initial Commit

---
+++ src/main/java/madp/user/MadpUserApplication.java
@@ -0,0 +1,13 @@
+package madp.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadpUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadpUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/madp/user/MadpUserApplicationTests.java
@@ -0,0 +1,13 @@
+package madp.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadpUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
